cfe9543a9e588be2d2617032b6a836dbb38cafd8
Balaji <tqexecution@gmail.com>
2020-09-09 17:07:02 +0530

    initial commit

---
+++ src/main/java/com/demo/tq/SpringBootWebAppExampleApplication.java
@@ -0,0 +1,13 @@
+package com.demo.tq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootWebAppExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootWebAppExampleApplication.class, args);
+	}
+
+}
